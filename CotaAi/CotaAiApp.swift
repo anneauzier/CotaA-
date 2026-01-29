8cb468f5763f0375c809476e0e0415353d82d605
anneauzier <anne.auziier@outlook.com>
2026-01-29 09:40:58 -0400

    chore: gitignore added

---
+++ CotaAi/CotaAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CotaAiApp.swift
+//  CotaAi
+//
+//  Created by Anne Auzier on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CotaAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
